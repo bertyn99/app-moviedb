ecf070891aed81eb6c2fed0e857933604e972db5
bertyn99 <bboulikou@gmail.com>
2018-08-10 01:20:54 +0200

    add api themoviedb

--- app/services/themoviedb.rb
+++ app/services/themoviedb.rb
@@ -1,1 +1,16 @@
-Tmdb::Api.key(ENV[:moviedb_api_key])
+require'dotenv-rails'
+Dotenv.load
+class Themoviedb
+    attr_accessor :client
+def initialize
+ @client= Tmdb::Api.key(ENV[:moviedb_api_key])
+end
+
+def language
+    Tmdb::Api.language("fr")
+end
+
+def perform
+    Tmdb::Movie.find("batman")
+end
+end
